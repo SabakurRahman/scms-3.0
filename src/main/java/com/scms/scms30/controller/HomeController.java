1cef4b5457aa2a5e2d80e329e1c504833e6f62a7
SabakurRahman <sabakur12360@gmail.com>
2024-07-30 23:21:20 +0600

    test page add

--- src/main/java/com/scms/scms30/controller/HomeController.java
+++ src/main/java/com/scms/scms30/controller/HomeController.java
@@ -1,5 +1,18 @@
 package com.scms.scms30.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
     
 }
