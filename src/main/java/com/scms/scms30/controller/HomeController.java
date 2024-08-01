119e41679e2da0ec171aeae87403b823db5e4c04
SabakurRahman <sabakur12360@gmail.com>
2024-08-02 01:28:18 +0600

    template mastering

--- src/main/java/com/scms/scms30/controller/HomeController.java
+++ src/main/java/com/scms/scms30/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.scms.scms30.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -10,8 +12,14 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
     @GetMapping("/test")
-    public String test() {
+    public String test(Model model) {
+        model.addAttribute("google_link", "https://www.google.com");
         return "test";
     }
     
